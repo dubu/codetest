b666f3c72beadecd422359e8e00d12dcaefcbe4c
rigel <rigel.k@kakaocorp.com>
2016-11-03 11:17:00 +0900

    Add 3 부터 한계인가.

--- src/test/java/com/dubu/ncalcTest.java
+++ src/test/java/com/dubu/ncalcTest.java
@@ -61,7 +61,7 @@ print convert(233, 16)
 
     private Integer revert(String S, int base) {
 
-        {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+//        {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
         char[] chars = S.toCharArray();
         for (int i = 0; i < chars.length; i++) {
