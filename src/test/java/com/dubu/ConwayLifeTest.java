a13cdbe381fbc4f6b2e301f290118c59a2353818
rigel <rigel.k@kakaocorp.com>
2016-11-17 18:37:56 +0900

    Add 3

--- src/test/java/com/dubu/ConwayLifeTest.java
+++ src/test/java/com/dubu/ConwayLifeTest.java
@@ -11,6 +11,8 @@ package com.dubu;
 
 import org.junit.Test;
 
+import java.net.SocketTimeoutException;
+
 import static org.junit.Assert.assertTrue;
 
 public class ConwayLifeTest{
@@ -34,8 +36,8 @@ public class ConwayLifeTest{
         int[][] res = ConwayLife.getGeneration(gliders[0], 1);
 
 
-        System.out.println(res);
-        //assertTrue("Got \n" + LifeDebug.htmlize(res) + "\ninstead of\n" + LifeDebug.htmlize(gliders[1]), LifeDebug.equals(res, gliders[1]));
+//        System.out.println(res);
+        assertTrue("Got \n" + LifeDebug.htmlize(res) + "\ninstead of\n" + LifeDebug.htmlize(gliders[1]), LifeDebug.equals(res, gliders[1]));
 
 
 //        assertTrue("hello world", true);
@@ -189,17 +191,75 @@ public class ConwayLifeTest{
  }
 
 class LifeDebug {
+    static int LIVE =1;
+    static int DIE = 0;
 
+    public static void print(int[][] cells) {
+        int lenY = cells.length;
+        int lenX = cells[0].length;
 
-    public static void print(int[][] glider) {
+        for (int j = 0; j <lenY; j++) {
+
+            for (int i = 0; i <lenX; i++) {
+
+                System.out.print(cells[j][i]);
+            }
+
+            System.out.println("");
+        }
 
     }
 
-    public static String htmlize(int[][] res) {
-        return null;
+    public static String htmlize(int[][] cells) {
+
+        StringBuffer sb = new StringBuffer();
+        int lenY = cells.length;
+        int lenX = cells[0].length;
+
+        for (int j = 0; j <lenY; j++) {
+
+            for (int i = 0; i <lenX; i++) {
+
+                int i1 = cells[j][i];
+                if(i1 == LIVE){
+                    //System.out.println("▓▓");
+                    sb.append("▓▓");
+
+                }else if (i1 == DIE){
+                    sb.append("░░");
+                    //System.out.println("░░");
+
+                }
+//                System.out.print(cells[j][i]);
+            }
+
+            //System.out.println("");
+            sb.append("\n");
+        }
+
+
+        return sb.toString();
     }
 
-    public static boolean equals(int[][] res, int[][] glider) {
-        return false;
+    public static boolean equals(int[][] res, int[][] cells) {
+
+
+        boolean isEqual = true;
+        int lenY = cells.length;
+        int lenX = cells[0].length;
+
+        for (int j = 0; j <lenY; j++) {
+
+            for (int i = 0; i < lenX; i++) {
+                int i1 = cells[j][i];
+                int i2 = cells[j][i];
+                if(i1 != i2){
+                    isEqual = false;
+                    return isEqual;
+                }
+
+            }
+        }
+        return isEqual;
     }
 }
