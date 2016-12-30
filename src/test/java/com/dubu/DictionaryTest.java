5b446d23d7511a6b047c67ef619a34eb70833abb
rigel <rigel.k@kakaocorp.com>
2016-12-30 17:02:34 +0900

    Add Dictionary

--- src/test/java/com/dubu/DictionaryTest.java
+++ src/test/java/com/dubu/DictionaryTest.java
@@ -9,9 +9,57 @@ package com.dubu;
  */
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.Assert.assertEquals;
 
+class Dictionary {
+    private final String[] words;
+
+    public Dictionary(String[] words) {
+        this.words = words;
+    }
+
+    public String findMostSimilar(String to) {
+
+        // exatly same
+
+
+        // count word
+
+
+        // positon
+
+        return null;
+    }
+}
+
 public class DictionaryTest {
+
+    @Test
+    public void testRegx() throws Exception {
+//        String str = "cherry";
+        String[] strings = {"cherry", "pineapple", "melon", "strawberry", "raspberry"};
+
+        for (int i = 0; i < strings.length; i++) {
+            String str = strings[i];
+
+            Pattern pattern = Pattern.compile("[^berry]*([berry]*)[^berry]*");
+            Matcher matcher = pattern.matcher(str);
+
+            while(matcher.find() && matcher.group(1).length() > 0){
+//            System.out.println(matcher.group());
+//            System.out.println(matcher.group(0));
+                if ((matcher.groupCount() > 0)) {
+
+                    System.err.println(String.format("%s %s %s %s ",str, matcher.group(1) ,matcher.group(1).length()  ,str.length()- matcher.group(1).length()));
+                }
+
+            }
+        }
+
+    }
 
     @Test
     public void testBerries() {
@@ -27,18 +75,5 @@ public class DictionaryTest {
         assertEquals("javascript", dictionary.findMostSimilar("javascript"));
     }
 
-}
-
-class Dictionary {
-    private final String[] words;
-
-    public Dictionary(String[] words) {
-        this.words = words;
-    }
-
-    public String findMostSimilar(String to) {
-        // TODO: this is your task ;)
-        return null;
-    }
 }
 
