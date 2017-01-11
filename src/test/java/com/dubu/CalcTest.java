92f80b472ca7ea489442a1b42f01f8d0f5e3868a
rigel <rigel.k@kakaocorp.com>
2017-01-11 18:59:48 +0900

    Add Calc

---
+++ src/test/java/com/dubu/CalcTest.java
@@ -0,0 +1,56 @@
+package com.dubu;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by rigel on 1/11/17.
+ */
+class Calc {
+
+    public double evaluate(String expr) {
+        // TODO: Your awesome code here
+        return 0;
+    }
+}
+
+
+public class CalcTest {
+
+    private Calc calc = new Calc();
+
+    @Test
+    public void shouldWorkWithEmptyString() {
+        assertEquals("Should work with empty string", 0, calc.evaluate(""), 0);
+    }
+
+    @Test
+    public void shouldParseNumbers() {
+        assertEquals("Should parse numbers", 3, calc.evaluate("1 2 3"), 0);
+    }
+
+    @Test
+    public void shouldParseFloatNumbers() {
+        assertEquals("Should parse float numbers", 3.5, calc.evaluate("1 2 3.5"), 0);
+    }
+
+    @Test
+    public void shouldSupportAddition() {
+        assertEquals("Should support addition", 4, calc.evaluate("1 3 +"), 0);
+    }
+
+    @Test
+    public void shouldSupportMultiplication() {
+        assertEquals("Should support multiplication", 3, calc.evaluate("1 3 *"), 0);
+    }
+
+    @Test
+    public void shouldSupportSubstraction() {
+        assertEquals("Should support substraction", -2, calc.evaluate("1 3 -"), 0);
+    }
+
+    @Test
+    public void shouldSupportDivision() {
+        assertEquals("Should support division", 2, calc.evaluate("4 2 /"), 0);
+    }
+}
