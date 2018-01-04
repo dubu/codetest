16a5bb79536b9e5eeef4fbf394f63a515f5efc2a
rigel <rigel.k@kakaocorp.com>
2018-01-04 11:43:58 +0900

    dy

---
+++ src/test/java/com/kakao/R1Test.java
@@ -0,0 +1,18 @@
+package com.kakao;
+
+import org.junit.Test;
+
+public class R1Test {
+
+
+    @Test
+    public void AsomeObj() {
+
+
+    }
+
+    public int solution(int n1, int[][] g1, int n2, int[][] g2) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ src/test/java/greek/GreekTree.java
@@ -0,0 +1,82 @@
+package greek;
+
+import org.junit.Test;
+
+public class GreekTree {
+
+
+    /**
+     *
+     *
+     *
+     * tree size
+     * https://www.geeksforgeeks.org/write-a-c-program-to-calculate-size-of-a-tree/
+     *
+     *
+     *
+     *
+     *
+     */
+    @Test
+    public void treeTest() {
+
+        /* creating a binary tree and entering the nodes */
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        System.out.println("The size of binary tree is : " + tree.size()); //5
+    }
+
+    private class BinaryTree {
+        public Node root;
+
+        public int size() {
+
+            if (root == null) {
+                return 0;
+            }
+
+
+            int sum = 1;
+            Node n = root;
+
+            while(n != null){
+
+                Node l = n.left;
+                Node r = n.right;
+
+                if(l != null){
+                   sum = sum+ countNode(l);
+                }
+                n =r;
+            }
+
+//            System.out.printf(sum);
+            System.out.println(sum);
+            return sum;
+        }
+    }
+
+    private int countNode(Node nNode) {
+        int count = 0 ;
+        while (nNode != null){
+            count = count + 1;
+            nNode = nNode.left;
+        }
+        return count;
+    }
+
+    private class Node {
+        public Node left;
+        public Node right;
+
+        public Node(int i) {
+
+
+        }
+    }
+}
