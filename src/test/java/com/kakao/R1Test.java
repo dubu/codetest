56cced885fd3f748d988d2bb6d5a62ddd62734cb
rigel <rigel.k@kakaocorp.com>
2018-01-04 19:02:17 +0900

    4 high sound

--- src/test/java/com/kakao/R1Test.java
+++ src/test/java/com/kakao/R1Test.java
@@ -112,8 +112,13 @@ public class R1Test {
         for (int i = 0; i < m; i++) {
 
             for (int j = 0; j < n; j++) {
-
+                checkTable.add(new int[]{i,j});
             }
+        }
+
+        while (checkTable.size() >0){
+            int simbol = (int) checkTable.remove(0);
+
         }
 
 
